1f132dd475a2c4fab493dea4c49bf3006620ed68
Daniel Markham <DanielBMarkham@Hotmail.com>
2017-12-12 18:36:19 -0600

    First commit

---
+++ console1/Helpers.fs
@@ -0,0 +1,125 @@
+﻿module Helpers
+    open System
+    //
+    // Program Command Line Config Settings
+    //
+    let getMatchingParameters (args:string []) (symbol:string) = 
+        args |> Array.filter(fun x->
+                    let argParms = x.Split([|':'|],2)
+                    let parmName = (argParms.[0]).Substring(1).ToUpper()
+                    if argParms.Length > 0 then parmName=symbol.ToUpper() else false
+                    )
+    let getValuePartOfMostRelevantCommandLineMatch (args:string []) (symbol:string) =
+        let matchingParms = getMatchingParameters args symbol
+        if matchingParms.Length > 0
+            then
+                // if there are multiple entries, last one overrides the rest
+                let commandLineParm = matchingParms.[matchingParms.Length-1]
+                let parmSections=commandLineParm.Split([|':'|], 2)
+                if parmSections.Length<2 then Some "" else Some parmSections.[1]
+            else
+                None
+
+    /// Parameterized type to allow command-line argument processing without a lot of extra coder work
+    /// Instantiate the type with the type of value you want. Make a default entry in case nothing is found
+    /// Then call the populate method. Will pull from args and return a val and args with the found value (if any consumed)
+    type ConfigEntry<'A> =
+        {
+            commandLineParameterSymbol:string
+            commandLineParameterName:string
+            parameterHelpText:string[]
+            parameterValue:'A
+        } with
+            member this.printVal =
+                printfn "%s: %s" this.commandLineParameterName (this.parameterValue.ToString())
+            member this.printHelp =
+                printfn "%s" this.commandLineParameterName
+                this.parameterHelpText |> Seq.iter(System.Console.WriteLine)
+            member this.swapInNewValue x =
+                {this with parameterValue=x}
+            static member populateValueFromCommandLine ((defaultConfig:ConfigEntry<string>), (args:string[])):ConfigEntry<string>  =
+                let parmValue = getValuePartOfMostRelevantCommandLineMatch args defaultConfig.commandLineParameterSymbol
+                if parmValue.IsSome
+                    then
+                        defaultConfig.swapInNewValue parmValue.Value
+                    else
+                        defaultConfig
+            static member populateValueFromCommandLine ((defaultConfig:ConfigEntry<int>), (args:string[])):ConfigEntry<int>  =
+                let parmValue = getValuePartOfMostRelevantCommandLineMatch args defaultConfig.commandLineParameterSymbol
+                if parmValue.IsSome
+                    then
+                        let parmInt = System.Int32.Parse("0" + parmValue.Value)
+                        defaultConfig.swapInNewValue parmInt
+                    else
+                        defaultConfig
+                                                 // All programs have at least this configuration on the command line
+    [<NoComparison>]
+    type ConfigBase =
+        {
+            programName:string
+            programTagLine:string
+            programHelpText:string[]
+        }
+        member this.printProgramDescription =
+            this.programHelpText |> Seq.iter(System.Console.WriteLine)
+        member this.printThis =
+            printfn "%s" this.programName
+            this.programHelpText |> Seq.iter(System.Console.WriteLine)
+
+    /// Command-line parameters for this particular (easyam) program
+    [<NoComparison>]
+    type console1ProgramConfig =
+        {
+            configBase:ConfigBase
+            messageToOutput:ConfigEntry<string>
+            numberOfTimesToRepeat:ConfigEntry<int>
+        }
+        member this.printThis() =
+            printfn "console1 Config Parameters Provided"
+            printfn "message to output: %s" this.messageToOutput.parameterValue
+            printfn "number of times to repeat the output: %d" this.numberOfTimesToRepeat.parameterValue
+    let createNewBaseOptions programName programTagLine programHelpText =
+        {
+            programName = programName
+            programTagLine = programTagLine
+            programHelpText=programHelpText
+        }
+
+    let createNewConfigEntry commandlineSymbol commandlineParameterName parameterHelpText initialValue =
+        {
+            commandLineParameterSymbol=commandlineSymbol
+            commandLineParameterName=commandlineParameterName
+            parameterHelpText=parameterHelpText
+            parameterValue=initialValue
+        }
+
+    /// Are we running on linux?
+    let isLinuxFileSystem =
+        let os = Environment.OSVersion
+        let platformId = os.Platform
+        match platformId with
+            | PlatformID.Win32NT | PlatformID.Win32S | PlatformID.Win32Windows | PlatformID.WinCE | PlatformID.Xbox -> false
+            | PlatformID.MacOSX | PlatformID.Unix -> true
+            | _ ->false
+    /// OS-independent file copy from one place to another. Uses shell.
+    let copyToDestinationDirectory (localFileName:string) (copyTo:string) =
+        if System.IO.File.Exists(localFileName) = false
+            then
+                ()
+            else
+                if not isLinuxFileSystem
+                    then
+                        let systemProc = new System.Diagnostics.Process()
+                        systemProc.EnableRaisingEvents<-false
+                        systemProc.StartInfo.FileName<-"cmd.exe"
+                        systemProc.StartInfo.Arguments<-("/C copy " + localFileName + " " + copyTo)
+                        systemProc.Start() |> ignore
+                        systemProc.WaitForExit()                
+                    else
+                        let systemProc = new System.Diagnostics.Process()
+                        systemProc.EnableRaisingEvents<-false
+                        systemProc.StartInfo.FileName<-"/bin/cp"
+                        systemProc.StartInfo.Arguments<-(" " + localFileName + " " + copyTo)
+                        //System.Console.WriteLine (systemProc.StartInfo.FileName + systemProc.StartInfo.Arguments)
+                        systemProc.Start() |> ignore
+                        systemProc.WaitForExit()
---
+++ console1/Program.fs
@@ -0,0 +1,50 @@
+﻿open Helpers
+    exception UserNeedsHelp of string
+    let programHelp = 
+        [|
+            "This program prints an important message a number of times."
+            ;"The default message is 'hello world.' The default number of times is 1."
+        |]
+    
+    let defaultBaseOptions = createNewBaseOptions "console1" "Important Message Printer" programHelp
+    let defaulMessageToOutput = createNewConfigEntry "M" "Message to print (Optional)" [|"/M:<message> -> This is the message you want printed."|] "Hello World"
+    let defaultNumberOfTimesToRepeat = createNewConfigEntry "N" "Number of times to print (Optional)" [|"/N:<integer> -> How many times you want it printed."|] 1
+    
+    let loadConfigFromCommandLine (args:string []):console1ProgramConfig =
+        if args.Length>0 && (args.[0]="?"||args.[0]="/?"||args.[0]="-?"||args.[0]="--?"||args.[0]="help"||args.[0]="/help"||args.[0]="-help"||args.[0]="--help") then raise (UserNeedsHelp args.[0]) else
+        let newConfigBase = defaultBaseOptions
+        let newMessageToOutput= ConfigEntry<_>.populateValueFromCommandLine(defaulMessageToOutput,args)
+        let newNumberOfTimesToRepeat=ConfigEntry<_>.populateValueFromCommandLine(defaultNumberOfTimesToRepeat,args)
+        { 
+            configBase = newConfigBase
+            messageToOutput=newMessageToOutput
+            numberOfTimesToRepeat=newNumberOfTimesToRepeat
+        }
+    
+    let doStuff (opts:console1ProgramConfig)=
+        let messagePrint()=printfn "%s" opts.messageToOutput.parameterValue
+        List.replicate(opts.numberOfTimesToRepeat.parameterValue) 0 |> List.iter(fun x->messagePrint())
+
+[<EntryPoint>]
+let main argv = 
+//    printfn "%A" argv
+//    0 // return an integer exit code
+        try
+            let opts = loadConfigFromCommandLine argv
+            doStuff opts 
+            0
+        with
+            | :? UserNeedsHelp as hex ->
+                defaultBaseOptions.printThis
+                0
+            | ex ->
+                System.Console.WriteLine ("Program terminated abnormally " + ex.Message)
+                System.Console.WriteLine (ex.StackTrace)
+                if ex.InnerException = null
+                    then
+                        0
+                    else
+                        System.Console.WriteLine("---   Inner Exception   ---")
+                        System.Console.WriteLine (ex.InnerException.Message)
+                        System.Console.WriteLine (ex.InnerException.StackTrace)
+                        0
